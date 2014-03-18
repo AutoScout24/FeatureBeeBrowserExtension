bedaba42535aee06abd1d6580ff3f31cfb44b2ee
Gleidson <gthuller@gmail.com>
2014-03-18 21:04:49 +0100

    connection to feature bee (adjustment needed)

--- Chrome/FeatureBeeToggleRepository.js
+++ Chrome/FeatureBeeToggleRepository.js
@@ -2,62 +2,55 @@
 
     this.retrieveCurrentToggles = function (callback) {
       
-        FeatureBeeTogglesExtensionStorage.retrieveTogglePersonalConfig(function(overwrittenToggles) {
+        FeatureBeeTogglesExtensionStorage.retrieveTogglePersonalConfig(function (overwrittenToggles) {
+            var retrieveFeaturesBasedOnPathDelegate = function(currentUrl) {
 
-            var currentToggles;
+                var parser = document.createElement('a');
+                parser.href = currentUrl;
+                var featurebeeFeaturesPath = parser.host + '/featurebee.axd/features';
+                alert(featurebeeFeaturesPath);
 
-            currentToggles = [
-            {
-                id: 1,
-                name: "Hi, I'm a toggle",
-                isActive: true,
-                status: "In Development",
-                team: "Dealer"
-            },
-            {
-                id: 2,
-                name: "Hi! I'm a off toggle",
-                isActive: false,
-                status: "In Development",
-                team: "Dealer"
-            },
-            {
-                id: 3,
-                name: "I'm a toggle and I feel goooood",
-                isActive: true,
-                status: "Released",
-                team: "ASM Endkunde"
-            },
-            {
-                id: 4,
-                name: "I'm a toggle on vacation. That's why I'm set to off. In addition, i have a very very long name.",
-                isActive: false,
-                status: "Under Test",
-                team: "ASM Endkunde"
-            }
-            ];
+                var request = new XMLHttpRequest();
+                request.open('GET', featurebeeFeaturesPath, false); 
+                request.send(null);
 
-            console.log("Overwritten:");
-            console.log(overwrittenToggles);
+                var currentToggles = [];
 
-            for (var i in overwrittenToggles) {
-                var toggle = overwrittenToggles[i];
-                for (var t in currentToggles) {
-                    var currentToggle = currentToggles[t];
-                    if (currentToggle.id == toggle.id) {
-                        if (currentToggle.status != toggle.status) {
-                            FeatureBeeTogglesExtensionStorage.persistTogglePersonalConfig(toggle);
-                        } else {
-                            currentToggles[t] = toggle;
-                        }                        
+                if (request.status === 200) {
+                    currentToggles = eval(request.responseText);
+                }
+
+                console.log("Overwritten:");
+                console.log(overwrittenToggles);
+
+                for (var i in overwrittenToggles) {
+                    var toggle = overwrittenToggles[i];
+                    for (var t in currentToggles) {
+                        var currentToggle = currentToggles[t];
+                        if (currentToggle.id == toggle.id) {
+                            if (currentToggle.status != toggle.status) {
+                                FeatureBeeTogglesExtensionStorage.persistTogglePersonalConfig(toggle);
+                            } else {
+                                currentToggles[t] = toggle;
+                            }
+                        }
                     }
                 }
+
+                console.log(currentToggles);
+                console.log("now calling: ");
+                console.log(callback);
+                callback(currentToggles);
+            };
+
+            if (chrome.tabs) {
+                chrome.tabs.query({ currentWindow: true, active: true }, function (currentTab) {
+                    retrieveFeaturesBasedOnPathDelegate(currentTab[0].url);
+                });
+            } else {
+                retrieveFeaturesBasedOnPathDelegate(document.URL);
             }
-            console.log(currentToggles);
-            console.log("now calling: ");
-            console.log(callback);
-            callback(currentToggles);
-        });        
+        });          
     };
 
     this.retrieveCurrentActiveToggles = function(callback) {
---
+++ Chrome/test_data.js
@@ -0,0 +1,74 @@
+﻿[
+    {
+        Name: "PMVM-2435",
+        Team: "Dealer",
+        State: "Released",
+        Enabled: true
+    },
+    {
+        Name: "CLLayout",
+        Team: "Dealer",
+        State: "Under Test",
+        Enabled: true
+    },
+    {
+        Name: "PMVM-2557 - Responsive List",
+        Team: "ASM Enduser",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "GuidedTourButton",
+        Team: "Dealer",
+        State: "Under Test",
+        Enabled: true
+    },
+    {
+        Name: "PMVM-2332 - Improving SEO for Lists/Details",
+        Team: "ASM Garage",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "HTML5ImageUpload",
+        Team: "Dealer",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "AverageQuality",
+        Team: "Dealer",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "PMVM1078",
+        Team: "Dealer",
+        State: "Released",
+        Enabled: true
+    },
+    {
+        Name: "New Audacon Import",
+        Team: "ASM Enduser",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "PMVM-2496-AdjustClassifiedListToggle",
+        Team: "Dealer",
+        State: "In Development",
+        Enabled: false
+    },
+    {
+        Name: "AveragePriceNew",
+        Team: "Dealer",
+        State: "Under Test",
+        Enabled: true
+    },
+    {
+        Name: "PMVM-2407",
+        Team: "Dealer",
+        State: "Under Test",
+        Enabled: true
+    }
+];
