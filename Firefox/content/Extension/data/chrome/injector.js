c23473abce16489e218048c59804731577844843
Gleidson <gthuller@gmail.com>
2014-09-03 22:02:47 +0200

    Clean up + joining code for chrome and firefox

--- Firefox/content/Extension/data/chrome/injector.js
+++
@@ -1,9 +0,0 @@
-﻿chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.executeScript({
-        file: 'data/js/Repository.js'
-    });
-
-    chrome.tabs.executeScript({
-        file: 'data/js/Presenter.js'
-    });
-});
